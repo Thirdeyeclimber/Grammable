1379c505b5cef1203c0d8758bbe0ff67138fa8c6
Michael <mr.enviro@gmail.com>
2019-09-05 18:55:26 +0000

    adding grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -5,4 +5,16 @@ class GramsController < ApplicationController
 
 	def index
 	end
+
+	def create
+		@gram = Gram.create(gram_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def gram_params
+		params.require(:gram).permit(:message)
+	end
+
 end
