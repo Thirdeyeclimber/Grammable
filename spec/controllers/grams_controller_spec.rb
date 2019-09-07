e8471165cd0609b996b85c212c2adae0571320f6
Michael <mr.enviro@gmail.com>
2019-09-07 02:40:32 +0000

    Editing grams

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -2,6 +2,21 @@ require 'rails_helper'
 
 # Gram Controller Specs
 RSpec.describe GramsController, type: :controller do
+  
+  # Edit action Tests
+  describe "grams#edit action" do
+    it "should successfully show the edit form if the gram is found" do
+      gram = FactoryBot.create(:gram)
+      get :edit, params: { id: gram.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should return a 404 error message if the gram is not found" do
+      get :edit, params: { id: 'SWAG' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
   # Show action Tests
   describe "grams#show action" do
     it "should successfully show the page if the gram is found" do
