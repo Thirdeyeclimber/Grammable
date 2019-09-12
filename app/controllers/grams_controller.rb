5fac1e1d3a427dd4af5e69af263e16ef0826ffb4
Michael <mr.enviro@gmail.com>
2019-09-12 21:55:56 +0000

    Adding Comments - passing all TDD tests

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -58,8 +58,5 @@ class GramsController < ApplicationController
 		params.require(:gram).permit(:message, :picture)
 	end
 
-  def render_not_found(status=:not_found)
-    render plain: "#{status.to_s.titleize} :(", status: status
-  end
 
 end
